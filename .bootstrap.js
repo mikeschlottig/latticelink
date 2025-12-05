babc39eb923e7305baa051997296fa9de52c028e
Vibesdk <vibesdk-bot@cloudflare.com>
2025-12-05 17:21:45 +0000

    chore: Update bootstrap script with latest commands

--- .bootstrap.js
+++ .bootstrap.js
@@ -77,7 +77,9 @@ function updateWranglerJsonc() {
 }
 
 function runSetupCommands() {
-    const commands = [];
+    const commands = [
+    "bun add node-fetch@^3.3.2"
+];
     
     if (commands.length === 0) {
         console.log('⊘ No setup commands to run');
